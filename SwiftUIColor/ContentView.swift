4cbd3f8b81a098c7de69d100d15b2d64fc997e09
Fatih Gumus <fatihgumus72@gmail.com>
2024-03-15 06:38:13 +0300

    color edit

--- SwiftUIColor/ContentView.swift
+++ SwiftUIColor/ContentView.swift
@@ -9,14 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Text("Fatih Gümüş")
+            .font(.headline)
+            .padding(10)
+            .foregroundColor(Color("AppColor"))
+        
+        RoundedRectangle(cornerRadius: 10)
+            .foregroundColor(Color("AppColor"))
+            .frame(width: 250,height: 150)
+            .shadow(color: Color.red, radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/,x: 0,y: 10)
     }
+    
 }
 
 #Preview {
