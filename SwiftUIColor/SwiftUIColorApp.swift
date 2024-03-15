3e0c90cc1faa0c84173eee3f17a20d20f65c8a84
Fatih Gumus <fatihgumus72@gmail.com>
2024-03-15 06:15:57 +0300

    Initial Commit

---
+++ SwiftUIColor/SwiftUIColorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIColorApp.swift
+//  SwiftUIColor
+//
+//  Created by Fatih Gümüş on 15.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIColorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
